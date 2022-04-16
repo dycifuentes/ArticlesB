beb29ea0c6983dda86142d5cae58ae6d799c9cca
Deyci Cifuentes <deyci.cifuentesp@gmail.com>
2022-04-16 16:49:06 -0600

    Use env file for enviroment variables and create rounting for the html pages

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
+require('dotenv').config()
 const {PAGE_NOT_EXIST} = require ('./constans.js');
 const app = express ();
 
 
 
 app.get('/', (req, res) => {
-    res.send('OK connection')
+    res.sendFile('./views/index.html', { root: __dirname});
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname});
+});
+
+app.get('/newarticle', (req, res) => {
+    res.sendFile('./views/newarticle.html', { root: __dirname});
+});
+
+
 
 app.get('*', (req, res) =>{
     res.status(404).send(PAGE_NOT_EXIST)
 });
 
-app.listen(3300, ()=>{
-    console.log('server is running in port 3300')
+const PORT = process.env.PORT || 3000
+
+app.listen( PORT, ()=>{
+    console.log(`server is running in port ${PORT}`)
 });
