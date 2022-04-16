b58968ff56f3bf27876c79d24c056a631a251fa7
Deyci Cifuentes <deyci.cifuentesp@gmail.com>
2022-04-16 17:21:54 -0600

    install ejs and create ejs files

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ require('dotenv').config()
 const {PAGE_NOT_EXIST} = require ('./constans.js');
 const app = express ();
 
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 });
 
 app.get('/newarticle', (req, res) => {
-    res.sendFile('./views/newarticle.html', { root: __dirname});
+    res.render('newarticle');
 });
-
-
 
 app.get('*', (req, res) =>{
     res.status(404).send(PAGE_NOT_EXIST)
