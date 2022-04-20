f2b9ca158b42352123fc03398c348941bb3dea4b
Deyci Cifuentes <deyci.cifuentesp@gmail.com>
2022-04-19 22:49:11 -0600

    create the schema and the model

---
+++ models/article.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    fragment: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
